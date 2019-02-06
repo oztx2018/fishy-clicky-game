82bd9605b650893b88bddb1c88e2ae3dbc92f6dc
oztx2018 <osmancaliskan64@gmail.com>
2019-02-05 18:43:09 -0600

    it works great

---
+++ fishy-clicky-game/src/components/Click/Click.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Click(props) {
+  return (
+    <div
+      role="img"
+      aria-label="click"
+      onClick={() => props.handleClick(props.id)}
+      style={{ backgroundImage: `url("${props.image}")` }}
+      className={`click${props.shake ? " shake" : ""}`}
+    />
+  );
+}
+
+export default Click;
---
+++ fishy-clicky-game/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        Fishy-Clicky Game!
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
